a7400ada8e11c5b5d17c07e37284b6f68867cda3
HAVRM <hiroki.mine.0614.163@gmail.com>
2017-04-20 05:30:03 +0900

    0420_0530_01

--- shell_script/auto_pdf/auto_get_pdf.sh
+++ shell_script/auto_pdf/auto_get_pdf.sh
@@ -23,8 +23,10 @@ then
 		for arg in ${DATA[@]}
 		do
 			echo ${arg}
-			. dropbox_uploader.sh upload ${arg} ${arg} >>result.txt 2>error.txt &
+			. dropbox_uploader.sh upload ${arg} ${arg} >>result.txt 2>terror.txt &
 			wait
+			cat terror.txt >>error.txt
+			rm -rf terror.txt
 		done
 		. write_little_endian.sh new_pdf.txt
 		. write_little_endian.sh new_pdf.txt "option_comand_by_-fup"
@@ -47,11 +49,18 @@ rm -rf result.txt error.txt >/dev/null 2>&1
 DATA=`date +%m%d/%H`
 echo $DATA >>result.txt
 echo "" >>result.txt
-. dropbox_uploader.sh download address_pdf.txt address_pdf.txt >>result.txt 2>error.txt &
-. dropbox_uploader.sh download new_pdf.txt new_pdf.txt >>result.txt 2>error.txt &
+. dropbox_uploader.sh download address_pdf.txt address_pdf.txt >>result.txt 2>terror.txt &
 wait
+cat terror.txt >>error.txt
+rm -rf terror.txt
+. dropbox_uploader.sh download new_pdf.txt new_pdf.txt >>result.txt 2>terror.txt &
+wait
+cat terror.txt >>error.txt
+rm -rf terror.txt
 NUM=0
 REN=0
+WGRE=0
+ERORNUM=0
 NAME=""
 ADDR=""
 DATA=(`cat address_pdf.txt`)
@@ -69,14 +78,25 @@ do
 		echo -e "${ADDR},\c"
 		if [ ${THE} = ${NAME} ]
 		then
-			until wget -O temp_${NAME} ${ADDR} >>result.txt 2>error.txt
+			until wget -O temp_${NAME} ${ADDR} >>result.txt 2>terror.txt
 			do
 				WGRE=$?
 				echo -e "${WGRE},\c"
+				cat terror.txt >>error.txt
+				rm -rf terror.txt
 				echo "${NAME}:${WGRE}" >>error.txt
+				ERORNUM=`expr ${ERORNUM} + 1`
+				if [ $ERORNUM -ge 5 ]
+				then
+					echo -e "fatal:\c"
+					echo "forced termination" >>error.txt
+					break
+				fi
+				WGRE=0
 				sleep 60s
 			done
-			echo "$?"
+			echo $WGRE
+			ERORNUM=0
 			NOW=$(wc -c ${NAME} | awk '{print $1}')
 			NEW=$(wc -c temp_${NAME} | awk '{print $1}')
 			if [ ${NOW} -ge ${NEW} ]
@@ -91,24 +111,41 @@ do
 					REN=1
 				fi
 				. write_little_endian.sh new_pdf.txt ${NAME}
-				. dropbox_uploader.sh upload ${NAME} ${NAME} >>result.txt 2>error.txt &
+				. dropbox_uploader.sh upload ${NAME} ${NAME} >>result.txt 2>terror.txt &
+				wait
+				cat terror.txt >>error.txt
+				rm -rf terror.txt
 			fi
 		else
-			until wget -O ${NAME} ${ADDR} >>result.txt 2>error.txt
+			until wget -O ${NAME} ${ADDR} >>result.txt 2>terror.txt
 			do
 				WGRE=$?
 				echo -e "${WGRE},\c"
+				cat terror.txt >>error.txt
+				rm -rf terror.txt
 				echo "${NAME}:${WGRE}" >>error.txt
+				ERORNUM=`expr ${ERORNUM} + 1`
+				if [ $ERORNUM -ge 5 ]
+				then
+					echo -e "fatal:\c"
+					echo "forced termination" >>error.txt
+					break
+				fi
+				WGRE=0
 				sleep 60s
 			done
-			echo "$?"
+			echo $WGRE
+			ERORNUM=0
 			if [ $REN = 0 ]
 			then
 				. write_little_endian.sh new_pdf.txt
 				REN=1
 			fi
 			. write_little_endian.sh new_pdf.txt "new:${NAME}"
-			. dropbox_uploader.sh upload ${NAME} ${NAME} >>result.txt 2>error.txt &
+			. dropbox_uploader.sh upload ${NAME} ${NAME} >>result.txt 2>terror.txt &
+			wait
+			cat terror.txt >>error.txt
+			rm -rf terror.txt
 		fi
 		NUM=0
 	else
@@ -125,10 +162,12 @@ then
 	. write_little_endian.sh new_pdf.txt ${DATA}
 fi
 . rm_line.sh
-. dropbox_uploader.sh upload new_pdf.txt new_pdf.txt >>result.txt 2>error.txt &
+. dropbox_uploader.sh upload new_pdf.txt new_pdf.txt >>result.txt 2>terror.txt &
 echo "waiting for finish uploading" >>result.txt
 echo "waiting for finish uploading"
 wait
+cat terror.txt >>error.txt
+rm -rf terror.txt
 . dropbox_uploader.sh upload error.txt error.txt >>/dev/null 2>&1 &
 . dropbox_uploader.sh upload result.txt result.txt >>/dev/null 2>&1 &
 echo "waiting for finish uploading report"
